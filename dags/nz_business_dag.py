e4939a7b4bfa8945d14e8e6f0034b6ae6d40a428
MacBook Pro <laura@MacBooks-MacBook-Pro.local>
2025-04-16 09:48:26 -0500

    last version

--- dags/nz_business_dag.py
+++ dags/nz_business_dag.py
@@ -1,17 +1,9 @@
 from airflow import models
+from airflow.operators.python import PythonOperator
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 from airflow.utils.dates import days_ago
 
 from google.cloud import storage
-
-def list_csv_files(bucket_name, prefix=''):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blobs = bucket.list_blobs(prefix=prefix)
-
-    csv_files = [blob.name for blob in blobs if blob.name.endswith('.csv')]
-    return csv_files
-
 
 BUCKET_NAME = 'nz_business_data_bucket'
 PROJECT_ID = 'nz-business-data-project'
@@ -23,30 +15,50 @@ default_args = {
 }
 
 with models.DAG(
-    'dag_auto_gcs_to_bq',
+    dag_id='gcs_to_bq_dynamic_loader',
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    
-    csv_files = list_csv_files(bucket_name=BUCKET_NAME, prefix=FOLDER)
+    def get_csv_file_names(**context):
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(BUCKET_NAME)
+        blobs = bucket.list_blobs(prefix=FOLDER)
 
-    # A single task for each file
-    for csv_file in csv_files:
-        task_id = f"load_{csv_file.replace('.csv', '').replace('/', '_')}"
-        table_name = csv_file.split('/')[-1].replace('.csv', '')
-        destination_table = f"{PROJECT_ID}.{DATASET}.{table_name}"
+        csv_files = [blob.name for blob in blobs if blob.name.endswith('.csv')]
+        context['ti'].xcom_push(key='csv_files', value=csv_files)
 
-        load_task = GoogleCloudStorageToBigQueryOperator(
-            task_id=task_id,
-            bucket=BUCKET_NAME,
-            source_objects=[csv_file],
-            source_format='CSV',
-            destination_project_dataset_table=destination_table,
-            skip_leading_rows=1,
-            autodetect=True,
-            write_disposition='WRITE_TRUNCATE',
-            create_disposition='CREATE_IF_NEEDED',
-            allow_jagged_rows=True,
-        )
+    list_csv_files = PythonOperator(
+        task_id='list_csv_files',
+        python_callable=get_csv_file_names,
+        provide_context=True,
+    )
+
+    def generate_tasks(**context):
+        csv_files = context['ti'].xcom_pull(key='csv_files', task_ids='list_csv_files')
+
+        for csv_file in csv_files:
+            table_name = csv_file.split('/')[-1].replace('.csv', '')
+            task_id = f"load_{table_name.replace('-', '_').replace('.', '_')}"
+
+            load_task = GoogleCloudStorageToBigQueryOperator(
+                task_id=task_id,
+                bucket=BUCKET_NAME,
+                source_objects=[csv_file],
+                destination_project_dataset_table=f"{PROJECT_ID}:{DATASET}.{table_name}",
+                source_format='CSV',
+                skip_leading_rows=1,
+                autodetect=True,
+                write_disposition='WRITE_TRUNCATE',
+            )
+
+            list_csv_files >> load_task
+
+    create_load_tasks = PythonOperator(
+        task_id='create_load_tasks',
+        python_callable=generate_tasks,
+        provide_context=True,
+    )
+
+    list_csv_files >> create_load_tasks
